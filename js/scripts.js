6bd4f1bbc0c8000e2e718624aec1d96998ae7748
Joanna Bieńkowska <ferart.pl@gmail.com>
2017-03-01 16:32:19 +0100

    zmiana input:text na input o konkretnym id i dodanie komentarzy

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,17 @@
 
         // T01Z02 - tydzien 1, zadanie 2
 
-       var allDivGrid = $("div.grid.grid-12"); // wszystkie elementy mające zarówno klasę grid i grid-12
+        var allDivGrid = $("div.grid.grid-12"); // wszystkie elementy mające zarówno klasę grid i grid-12
+ 
+        var externalLinks = $(".nav a[href^='http']"); // wszystkie linki kierujące na zewnątrz zawierające się w elemencie o klasie nav
 
-       var externalLinks = $(".nav a[href^='http']"); // wszystkie linki kierujące na zewnątrz zawierające się w elemencie o klasie nav
+        var notChecked = $("input:checkbox, input:radio").not(":checked"); // wszystkie elementy input type checkbox i radiobutton, które nie są zaznaczone
 
-       var notChecked = $("input:checkbox, input:radio").not(":checked"); // wszystkie elementy input type checkbox i radiobutton, które nie są zaznaczone
+        var firsPWithoutChild = $("div#text p:empty:first"); // wszystkie pierwsze paragrafy nie mające dzieci (nie będące rodzicami) zawierające się w divie z identyfikatorem text
 
-       var firsPWithoutChild = $("div#text p:empty:first"); // wszystkie pierwsze paragrafy nie mające dzieci (nie będące rodzicami) zawierające się w divie z identyfikatorem text
+        // var firstPWithoutChild = $("div#text p:first").not(:parent); - to inny sposob osiągnięcia tego samego efektu co wyżej
 
-       // var firstPWithoutChild = $("div#text p:first").not(:parent); - to inny sposob osiągnięcia tego samego efektu co wyżej
-
-       var paginationItemNotSpan = $(".pagination-item").not("span"); // wszystkie elementy z podaną klasą nie będące spanami
+        var paginationItemNotSpan = $(".pagination-item").not("span"); // wszystkie elementy z podaną klasą nie będące spanami
 
 
 
@@ -23,30 +23,37 @@
 
         //T01Z03
         
-        $(".menu-btn").on("click", function(){
+        // menu rozwijane
+        
+        $(".menu-btn").on("click", function(){ 
 
-          $(".nav").toggle("slow");
+            $(".nav").toggle("slow");
 
         }); 
 
         //T01Z04
         
         $("#name-form .add-btn").on("click", function(){ 
+
+            // wyłączenie natywnej funkcji buttona - zastąpiłam je przez type="button" w index.html
           
             // $( "form" ).submit(function( event ) {
             //   event.preventDefault();
             // });                       
             
-            if( $("#name-form input:text").val() == '' ) {
-                  
+            if( $("#name-form input#name").val() == '' ) { //<- spraawdzenie czy pole input id="name" jest puste
+                // jeśli jest - nic się nie dzieje
             }
             else {
                 
-                $("ul.name-list").append('<li>'+($('#name-form input').val())+'</li>');
+                // jeśli nie jest - zostaje wstawiona jego zawartość do ul jako kolejny punkt na liście
+                
+                $("ul.name-list").append('<li>'+($('#name-form input#name').val())+'</li>'); 
+
                 
             }
 
-            $("#name-form input:text").val(''); //czyści po kliknięciu input
+            $("#name-form input#name").val(''); //czyści po kliknięciu input
             
         });  
 
